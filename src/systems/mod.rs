57d8ca45b9ed6074d7e8446a8f9fbe9d654d2ba7
Luis Prates <lprates@student.42lisboa.com>
2022-10-23 22:08:13 +0100

    Chapter 8 finished - Health and Melee Combat

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -1,10 +1,12 @@
 mod player_input;
 mod map_render;
 mod entity_render;
-mod collisions;
 mod random_move;
 mod end_turn;
 mod movement;
+mod hud;
+mod tooltips;
+mod combat;
 
 use crate::prelude::*;
 
@@ -13,32 +15,37 @@ pub fn build_input_scheduler() -> Schedule {
 		.add_system(player_input::player_input_system())
 		.flush()
 		.add_system(map_render::map_render_system())
+		.add_system(hud::hud_system())
 		.add_system(entity_render::entity_render_system())
+		.add_system(hud::hud_system())
+		.add_system(tooltips::tooltips_system())
 		.build()
 }
 
 pub fn build_player_schedule() -> Schedule {
 	Schedule::builder()
-		.add_system(movement::movement_system())
+		.add_system(combat::combat_system())
 		.flush()
-		.add_system(collisions::collision_system())
+		.add_system(movement::movement_system())
 		.flush()
 		.add_system(map_render::map_render_system())
 		.add_system(entity_render::entity_render_system())
+		.add_system(hud::hud_system())
 		.add_system(end_turn::end_turn_system())
 		.build()
 }
 
 pub fn build_monster_scheduler() -> Schedule {
 	Schedule::builder()
+		.add_system(combat::combat_system())
+		.flush()
 		.add_system(random_move::random_move_system())
 		.flush()
 		.add_system(movement::movement_system())
 		.flush()
-		.add_system(collisions::collision_system())
-		.flush()
 		.add_system(map_render::map_render_system())
 		.add_system(entity_render::entity_render_system())
+		.add_system(hud::hud_system())
 		.add_system(end_turn::end_turn_system())
 		.build()
 }
