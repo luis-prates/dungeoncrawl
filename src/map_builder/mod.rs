9fc9aa927c831d6cba037c76503a42f97ff4191a
Luis Prates <lprates@student.42lisboa.com>
2023-07-27 23:37:53 +0100

    Adds theming and fixes ASCII tilesheet

--- src/map_builder/mod.rs
+++ src/map_builder/mod.rs
@@ -3,12 +3,14 @@ mod rooms;
 mod automata;
 mod drunkard;
 mod prefab;
+mod themes;
 
 use crate::prelude::*;
 use empty::EmptyArchitect;
 use rooms::RoomsArchitect;
 use automata::CellularAutomataArchitect;
 use prefab::apply_prefab;
+use themes::*;
 
 use self::drunkard::DrunkardsWalkArchitect;
 const NUM_ROOMS: usize = 20;
@@ -17,12 +19,17 @@ trait MapArchitect {
 	fn new(&mut self, rng: &mut RandomNumberGenerator) -> MapBuilder;
 }
 
+pub trait MapTheme : Sync + Send {
+	fn tile_to_render(&self, tile_type : TileType) -> FontCharType;
+}
+
 pub struct MapBuilder {
 	pub map : Map,
 	pub rooms : Vec<Rect>,
 	pub monster_spawns: Vec<Point>,
 	pub player_start : Point,
-	pub amulet_start : Point
+	pub amulet_start : Point,
+	pub theme: Box<dyn MapTheme>
 }
 
 impl MapBuilder {
@@ -151,6 +158,10 @@ impl MapBuilder {
 		};
 		let mut mb = architect.new(rng);
 		apply_prefab(&mut mb, rng);
+		mb.theme = match rng.range(0, 2) {
+			0 => DungeonTheme::new(),
+			_ => ForestTheme::new()
+		};
 		display("Final Map", &mb.map, &mb.player_start, &mb.amulet_start, &mb.monster_spawns);
 		mb
 	}
